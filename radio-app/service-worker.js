5740ac81d2b7ce93facd6dbab556fb502c40b329
Velvetverse <maxnomic@outlook.com>
2026-09-20 13:16:44 -0400

    Add VelvetVerse Radio player app

---
+++ radio-app/service-worker.js
@@ -0,0 +1,19 @@
+const CACHE='velvetverse-radio-player-v1';
+const LOCAL=['./','./index.html','./manifest.json','../logo.png','../icon-192.png','../icon-512.png','../apple-touch-icon.png','../adrian.png','../suzy.png','../carlos.png','../lara.png','../beto.png','../rui-freitas-velvetverse-experience.png','../velvetverse.png'];
+
+self.addEventListener('install',event=>{
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL)).then(()=>self.skipWaiting()));
+});
+
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+});
+
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin) return;
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  })));
+});
