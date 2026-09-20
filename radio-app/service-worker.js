8ac96a6d73b6dbbbfef1c2e97457d6bc4a7e0360
Velvetverse <maxnomic@outlook.com>
2026-09-20 13:40:15 -0400

    Add files via upload

--- radio-app/service-worker.js
+++ radio-app/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE='velvetverse-radio-player-v1';
+const CACHE='velvetverse-radio-player-v2';
 const LOCAL=['./','./index.html','./manifest.json','../logo.png','../icon-192.png','../icon-512.png','../apple-touch-icon.png','../adrian.png','../suzy.png','../carlos.png','../lara.png','../beto.png','../rui-freitas-velvetverse-experience.png','../velvetverse.png'];
 
 self.addEventListener('install',event=>{
@@ -11,6 +11,14 @@ self.addEventListener('activate',event=>{
 
 self.addEventListener('fetch',event=>{
   if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin) return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();
+      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+      return response;
+    }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
+    return;
+  }
   event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
     const copy=response.clone();
     caches.open(CACHE).then(cache=>cache.put(event.request,copy));
