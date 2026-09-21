6d9de3962d0c27fa2fdb8a2e108e99999459c089
Velvetverse <maxnomic@outlook.com>
2026-09-20 20:21:39 -0400

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,11 @@
-const CACHE='velvetverse-radio-player-v4';
-const LOCAL=['./','./index.html','./manifest.json','../logo.png','../icon-192.png','../icon-512.png','../apple-touch-icon.png','../adrian.png','../suzy.png','../carlos.png','../lara.png','../beto.png','../rui-freitas-velvetverse-experience.png','../velvetverse.png'];
-
-self.addEventListener('install',event=>{
-  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(LOCAL)).then(()=>self.skipWaiting()));
+self.addEventListener("install", function(event) {
+  self.skipWaiting();
 });
 
-self.addEventListener('activate',event=>{
-  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()));
+self.addEventListener("activate", function(event) {
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener('fetch',event=>{
-  if(event.request.method!=='GET'||new URL(event.request.url).origin!==location.origin) return;
-  if(event.request.mode==='navigate'){
-    event.respondWith(fetch(event.request).then(response=>{
-      const copy=response.clone();
-      caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-      return response;
-    }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match('./index.html'))));
-    return;
-  }
-  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
-    const copy=response.clone();
-    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
-    return response;
-  })));
+self.addEventListener("fetch", function(event) {
+  event.respondWith(fetch(event.request));
 });
